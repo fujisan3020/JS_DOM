f377e6b01e708a78312f5c6ce59527a751072095
fujisan <gashigashi3040@gmail.com>
2020-05-05 19:59:17 +0900

    #10 要素を追加してみよう

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
   //addEventListenerの第一引数はイベントの種類、第二引数は実行したい
   // 処理を関数やアロー関数で渡す
   document.querySelector('button').addEventListener('click', () => {
-    const targetNode = document.getElementById('target');
+    const item2 = document.createElement('li');
+    item2.textContent = 'item 2';
 
-    // targetNode.textContent = 'Dotinstall'
-    targetNode.textContent = targetNode.dataset.translation;
+    // const ulNode = document.querySelector('ul');
+    const ul = document.querySelector('ul');
+    ul.appendChild(item2);
     });
 }
