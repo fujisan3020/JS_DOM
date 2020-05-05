651a01674be0c83257f7098a50e41d77f85954cc
fujisan <gashigashi3040@gmail.com>
2020-05-05 21:06:02 +0900

    » #16 チェックボックスを操作してみよう

--- js/main.js
+++ js/main.js
@@ -5,16 +5,19 @@
   // 処理を関数やアロー関数で渡す
   document.querySelector('button').addEventListener('click', () => {
     const colors = document.querySelectorAll('input');
-    let selectedColor;
+    const selectedColors = [];
 
     colors.forEach(color => {
       if (color.checked) {
-        selectedColor = color.value;
+        //pushメソッド: 配列の末尾に値を入れるメソッド
+        selectedColors.push(color.value);
       }
     });
 
     const li = document.createElement('li');
-    li.textContent = selectedColor;
+    // li.textContent = selectedColors.join(',');
+    li.textContent = selectedColors;
     document.querySelector('ul').appendChild(li);
+
   });
 }
