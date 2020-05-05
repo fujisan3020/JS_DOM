2ffdba31cc5bf65b21a5b3f448f6b1e28e52253b
fujisan <gashigashi3040@gmail.com>
2020-05-05 22:40:54 +0900

    » #21 イベントの伝播を理解しよう

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
 {
-  document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault();
-    console.log('submit');
+  document.querySelector('ul').addEventListener('click', e => {
+
+    //e.target.nodeName では，xmlの場合はdivやliなど要素名をそのまま返しますが，htmlの場合は要素名を大文字にして返すため，判定に利用する文字もそれに併せて大文字にしています．
+    if (e.target.nodeName === 'LI') {
+      e.target.classList.toggle('done');
+    }
   });
 }
