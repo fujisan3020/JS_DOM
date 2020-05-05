9b1ed5c1bacf36f6a74578ca2d3cfd4da17af81a
fujisan <gashigashi3040@gmail.com>
2020-05-05 20:13:21 +0900

    » #11 要素の複製､ 挿入をしてみよう

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@
   //addEventListenerの第一引数はイベントの種類、第二引数は実行したい
   // 処理を関数やアロー関数で渡す
   document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');
-    item2.textContent = 'item 2';
+    const item0 = document.querySelectorAll('li')[0];
+    //cloneNode(): Nodeを複製する命令。
+    //            (true)で要素の中身も複製、(false)で中身は複製しない
+    const copy = item0.cloneNode(true);
 
-    // const ulNode = document.querySelector('ul');
     const ul = document.querySelector('ul');
-    ul.appendChild(item2);
-    });
+    const item2 = document.querySelectorAll('li')[2];
+    //insertBefore(): 何らかの要素を指定した要素の前に入れる
+    //                (入れる要素, どの要素の前に入れるか)
+    ul.insertBefore(copy, item2);
+  });
 }
