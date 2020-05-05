3b3c7c7cb7f5870be709f6eb4615ba9c903092a1
fujisan <gashigashi3040@gmail.com>
2020-05-05 18:07:28 +0900

    #08 classListを使ってみよう

--- js/main.js
+++ js/main.js
@@ -6,7 +6,21 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
 
+    //className: クラス名を変更する
     // targetNode.className = 'my-color';
-    targetNode.className = 'my-color my-border';
-  });
+
+    //classList.add: クラス名を追加してくれる
+    // targetNode.classList.add('my-color');
+
+    //calssList.cotains:特定のクラスがついているか判別する
+    //calssList.remove:特定のクラスを外す
+    // if (targetNode.classList.contains('my-color')) {
+    //   targetNode.classList.remove('my-color');
+    // } else {
+    //   targetNode.classList.add('my-color');
+    // }
+
+    //上記と同じ処理ができる
+    targetNode.classList.toggle('my-color');
+    });
 }
