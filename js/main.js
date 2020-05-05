1c3cd893a6b44aa7f3e55c8e7c352d7dddd0db96
fujisan <gashigashi3040@gmail.com>
2020-05-05 20:18:00 +0900

    要素の削除をしてみよう

--- js/main.js
+++ js/main.js
@@ -4,15 +4,10 @@
   //addEventListenerの第一引数はイベントの種類、第二引数は実行したい
   // 処理を関数やアロー関数で渡す
   document.querySelector('button').addEventListener('click', () => {
-    const item0 = document.querySelectorAll('li')[0];
-    //cloneNode(): Nodeを複製する命令。
-    //            (true)で要素の中身も複製、(false)で中身は複製しない
-    const copy = item0.cloneNode(true);
+    const item1 = document.querySelectorAll('li')[1];
 
-    const ul = document.querySelector('ul');
-    const item2 = document.querySelectorAll('li')[2];
-    //insertBefore(): 何らかの要素を指定した要素の前に入れる
-    //                (入れる要素, どの要素の前に入れるか)
-    ul.insertBefore(copy, item2);
+    // item1.remove();
+    // 親Node.removeChild(削除するNode)
+    document.querySelector('ul').removeChild(item1);
   });
 }
