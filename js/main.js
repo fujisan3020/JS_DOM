6c5bc31916be77ab2fe37f76264b92cfad91752f
fujisan <gashigashi3040@gmail.com>
2020-05-05 20:57:05 +0900

    » #15 ラジオボタンを操作してみよう

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@
   //addEventListenerの第一引数はイベントの種類、第二引数は実行したい
   // 処理を関数やアロー関数で渡す
   document.querySelector('button').addEventListener('click', () => {
+    const colors = document.querySelectorAll('input');
+    let selectedColor;
+
+    colors.forEach(color => {
+      if (color.checked) {
+        selectedColor = color.value;
+      }
+    });
+
     const li = document.createElement('li');
-    const color = document.querySelector('select');
-    li.textContent = `${color.value} - ${color.selectedIndex}`;
+    li.textContent = selectedColor;
     document.querySelector('ul').appendChild(li);
   });
 }
