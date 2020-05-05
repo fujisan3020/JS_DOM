ee1e1fb54f849ce391dccac40d314c2e2fa8a032
fujisan <gashigashi3040@gmail.com>
2020-05-05 20:45:09 +0900

    » #14 セレクトボックスを操作してみよう

--- js/main.js
+++ js/main.js
@@ -5,15 +5,8 @@
   // 処理を関数やアロー関数で渡す
   document.querySelector('button').addEventListener('click', () => {
     const li = document.createElement('li');
-
-    //inputで入力された値を取得
-    const text = document.querySelector('input');
-    //入力された値はvalue属性で取得できる
-    li.textContent = text.value;
+    const color = document.querySelector('select');
+    li.textContent = `${color.value} - ${color.selectedIndex}`;
     document.querySelector('ul').appendChild(li);
-
-    //入力欄をクリアし、フォーカスさせる
-    text.value = '';
-    text.focus();
   });
 }
