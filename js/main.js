79553b12ef9789e188403b480c9643f9708f95fb
fujisan <gashigashi3040@gmail.com>
2020-05-05 22:28:24 +0900

    » #20 フォームを送信してみよう

--- js/main.js
+++ js/main.js
@@ -1,25 +1,8 @@
 'use strict';
 
 {
-  const text = document.querySelector('textarea');
-
-  text.addEventListener('focus', () => {
-    console.log('focus');
-  });
-
-  //blur: フォーカスが外れた時のイベント
-  text.addEventListener('blur', () => {
-    console.log('blur');
-  });
-
-  //input : 内容が更新された時
-  text.addEventListener('input', () => {
-    // console.log('input');
-    console.log(text.value.length);
-  });
-
-  //change: 更新が確定した時
-  text.addEventListener('change', () => {
-    console.log('change');
+  document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('submit');
   });
 }
